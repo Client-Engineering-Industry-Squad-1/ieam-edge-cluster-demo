4d00d0e9f273f58e074f6458d86c51bce51b503f
jenn <jennifer.nguyen@ibm.com>
2023-06-23 00:21:02 -0700

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,20 +9,20 @@ APP_IMAGE=$APP_IMAGE_BASE:$IMAGE_VERSION
 OPERATOR_IMAGE_BASE="docker.io/jennuineness/ieam-edge-operator"
 OPERATOR_IMAGE=$OPERATOR_IMAGE_BASE:$IMAGE_VERSION
 
-cd $DEPLOY_DIR/config/manager && kustomize edit set image controller="$OPERATOR_IMAGE" && cd $DEPLOY_DIR
+cd $DEPLOY_DIR && git stash
+cd config/manager && kustomize edit set image controller="$OPERATOR_IMAGE" && cd ../..
 sed -i -e "s|{{APP_IMAGE_BASE}}|$APP_IMAGE_BASE|" config/samples/demo.yaml
 sed -i -e "s|{{IMAGE_VERSION}}|$IMAGE_VERSION|" config/samples/demo.yaml
 
-
 # Update Version in horizon/hzn.json if you make ANY change
-mv $DEPLOY_DIR/horizon/hzn.json /tmp/hzn.json
-jq --arg IMAGE_VERSION "$IMAGE_VERSION" '.MetadataVars.SERVICE_VERSION |= $IMAGE_VERSION' /tmp/hzn.json > $DEPLOY_DIR/horizon/hzn.json
+mv horizon/hzn.json /tmp/hzn.json
+jq --arg IMAGE_VERSION "$IMAGE_VERSION" '.MetadataVars.SERVICE_VERSION |= $IMAGE_VERSION' /tmp/hzn.json > horizon/hzn.json
 
-make -C $DEPLOY_DIR docker-build docker-push IMG=$OPERATOR_IMAGE
+make docker-build docker-push IMG=$OPERATOR_IMAGE
 
-rm $DEPLOY_DIR/operator.tar.gz && rm -rf $DEPLOY_DIR/deploy && mkdir $DEPLOY_DIR/deploy
-cd $DEPLOY_DIR && kustomize build config/default > deploy/kustomize_manifests_operator.yaml
-cd $DEPLOY_DIR && tar -C deploy -czf operator.tar.gz .
+rm operator.tar.gz & mkdir deploy
+kustomize build config/default > deploy/kustomize_manifests_operator.yaml
+tar -C deploy -czf operator.tar.gz . && rm -rf deploy
 
 # # rm operator.tar.gz && tar -czf operator.tar deploy && gzip operator.tar 
 hzn exchange service publish -f $DEPLOY_DIR/horizon/service.definition.json --overwrite
