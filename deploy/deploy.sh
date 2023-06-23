2853a582fc5432c54f26800364c4898fbf22ff36
jenn <jennifer.nguyen@ibm.com>
2023-06-22 20:22:52 -0700

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd ~/ieam-edge-cluster-demo/deploy
-. env.sh
+DEPLOY_DIR="~/ieam-edge-cluster-demo/deploy"
+. $DEPLOY_DIR/env.sh
 
 APP_IMAGE_BASE=$1
 IMAGE_VERSION=$2
@@ -9,21 +9,21 @@ APP_IMAGE=$APP_IMAGE_BASE:$IMAGE_VERSION
 OPERATOR_IMAGE_BASE="docker.io/jennuineness/ieam-edge-operator"
 OPERATOR_IMAGE=$OPERATOR_IMAGE_BASE:$IMAGE_VERSION
 
-cd config/manager && kustomize edit set image controller="$OPERATOR_IMAGE" && cd ../..
-cd config/samples && kustomize edit set image nginxinc/nginx-unprivileged="$APP_IMAGE" && cd ../..
+cd $DEPLOY_DIR/config/manager && kustomize edit set image controller="$OPERATOR_IMAGE" && cd $DEPLOY_DIR
+cd $DEPLOY_DIR/config/samples && kustomize edit set image nginxinc/nginx-unprivileged="$APP_IMAGE" && cd $DEPLOY_DIR
 
 # Update Version in horizon/hzn.json if you make ANY change
-mv horizon/hzn.json /tmp/hzn.json
-jq --arg IMAGE_VERSION "$IMAGE_VERSION" '.MetadataVars.SERVICE_VERSION |= $IMAGE_VERSION' /tmp/hzn.json > horizon/hzn.json
+mv $DEPLOY_DIR/horizon/hzn.json /tmp/hzn.json
+jq --arg IMAGE_VERSION "$IMAGE_VERSION" '.MetadataVars.SERVICE_VERSION |= $IMAGE_VERSION' /tmp/hzn.json > $DEPLOY_DIR/horizon/hzn.json
 
-make docker-build docker-push IMG=$OPERATOR_IMAGE
+make -C $DEPLOY_DIR docker-build docker-push IMG=$OPERATOR_IMAGE
 
-rm operator.tar.gz && rm -rf deploy && mkdir deploy
-kustomize build config/default > deploy/kustomize_manifests_operator.yaml
-tar -C deploy -czf operator.tar.gz .
+rm $DEPLOY_DIR/operator.tar.gz && rm -rf $DEPLOY_DIR/deploy && mkdir $DEPLOY_DIR/deploy
+kustomize build $DEPLOY_DIR/config/default > $DEPLOY_DIR/deploy/kustomize_manifests_operator.yaml
+tar -C $DEPLOY_DIR/deploy -czf operator.tar.gz $DEPLOY_DIR
 
 # # rm operator.tar.gz && tar -czf operator.tar deploy && gzip operator.tar 
-hzn exchange service publish -f horizon/service.definition.json
+hzn exchange service publish -f $DEPLOY_DIR/horizon/service.definition.json
 
 HZN_POLICY_NAME="samsung/policy-alb-ieam-edge-cluster-app"
-hzn exchange deployment addpolicy -f horizon/service.policy.json $HZN_POLICY_NAME
+hzn exchange deployment addpolicy -f $DEPLOY_DIR/horizon/service.policy.json $HZN_POLICY_NAME
